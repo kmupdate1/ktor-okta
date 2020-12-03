916a2fea7145d5997a092c4a230215f31d0da011
Ken <lax.potcx1562@gmail.com>
2020-12-04 01:26:48 +0900

    とりあえず動いたけど，ログインできない

--- src/auth-settings.kt
+++ src/auth-settings.kt
@@ -1,3 +1,37 @@
 package com.okta.demo.ktor
 
+import com.typesafe.config.Config
+import io.ktor.auth.*
+import io.ktor.config.*
+import io.ktor.http.HttpMethod.Companion.Post
+import io.ktor.util.*
 
+data class OktaConfig (
+    val orgUrl: String,
+    val clientId: String,
+    val clientSecret: String,
+    val audience: String
+) {
+    val accessTokenUrl = "$orgUrl/vl/token"
+    val authorizeUrl = "$orgUrl/vl/authorize"
+    val logoutUrl = "$orgUrl/vl/logout"
+}
+
+@KtorExperimentalAPI
+fun oktaConfigReader(config: Config): OktaConfig = OktaConfig(
+    orgUrl = config.getString("okta.orgUrl"),
+    clientId = config.getString("okta.clientId"),
+    clientSecret = config.getString("okta.clientSecret"),
+    audience = config.tryGetString("okta.audience")?: "api://default"
+)
+
+fun OktaConfig.asOAuth2Config(): OAuthServerSettings.OAuth2ServerSettings =
+    OAuthServerSettings.OAuth2ServerSettings(
+        name = "okta",
+        authorizeUrl = authorizeUrl,
+        accessTokenUrl = accessTokenUrl,
+        clientId = clientId,
+        clientSecret = clientSecret,
+        defaultScopes = listOf("openid", "profile"),
+        requestMethod = Post
+    )
--- src/auth.kt
+++ src/auth.kt
@@ -1,8 +1,76 @@
 package com.okta.demo.ktor
 
+import com.okta.jwt.JwtVerifiers
+import com.typesafe.config.ConfigFactory
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.client.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.sessions.*
+import io.ktor.util.*
 
-
+@KtorExperimentalAPI
 fun Application.setupAuth() {
-    // TODO: To be implemented
+    val oktaConfig = oktaConfigReader(ConfigFactory.load()?: throw Exception("Could not load config"))
+    val accessTokenVerifier = JwtVerifiers.accessTokenVerifierBuilder()
+        .setAudience(oktaConfig.audience)
+        .setIssuer(oktaConfig.orgUrl)
+        .build()
+    val idVerifier = JwtVerifiers.idTokenVerifierBuilder()
+        .setClientId(oktaConfig.clientId)
+        .setIssuer(oktaConfig.orgUrl)
+        .build()
+
+    install(Authentication) {
+        oauth {
+            urlProvider = { "http://localhost:8080/login/authorization-callback" }
+            providerLookup = { oktaConfig.asOAuth2Config() }
+            client = HttpClient()
+        }
+    }
+
+    routing {
+        authenticate {
+            // Okta calls this endpoint providing accessToken along with requested idToken
+            get("/login/authorization-callback") {
+                // Get a principal from OAuth2 token
+                val principal = call.authentication.principal<OAuthAccessTokenResponse.OAuth2>()
+                    ?: throw Exception("No principal was given")
+
+                // Parse and verify access token with Okta.JwtVerifier
+                val accessToken = accessTokenVerifier.decode(principal.accessToken)
+
+                // Get idTokenString, parse and verify id token
+                val idTokenString = principal.extraParameters["id_token"]
+                    ?: throw Exception("id_token wasn't returned")
+
+                val idToken = idVerifier.decode(idTokenString, null)
+
+                // try to get handle from the id token, of fail back to subject field in access token
+                val fullName = (idToken.claims["name"]?: accessToken.claims["sub"]?: "UNKNOWN_NAME").toString()
+                println("User $fullName logged in successfully")
+
+                // Create a session object with "slugified" username
+                val session = UserSession(
+                    username = fullName.replace("[^a-zA-Z0-9]".toRegex(), ""),
+                    idToken = idTokenString
+                )
+
+                call.sessions.set(session)
+                call.respondRedirect("/")
+            }
+
+            //When guest accessing '/login' it automatically redirects to okta login page
+            get("/login") {
+                call.respondRedirect("/")
+            }
+        }
+
+        // Perform logout by cleaning cookies
+        get("/logout") {
+            call.sessions.clear<UserSession>()
+            call.respondRedirect("/")
+        }
+    }
 }
